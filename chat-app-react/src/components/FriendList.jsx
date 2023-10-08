c778d437d3fe3fe07049f397f1856377d5bfcab5
Chai Phonbopit <Phonbopit@gmail.com>
2023-10-08 16:06:42 +0700

    chore: remove log and strictMode

--- chat-app-react/src/components/FriendList.jsx
+++ chat-app-react/src/components/FriendList.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 const FriendList = ({ friends }) => {
-  console.log('frrrrrrrrrrr', friends)
   return (
     <div id="chat-friend-list">
       <h3>Friends</h3>
